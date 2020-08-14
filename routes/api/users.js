2cbab4a3b0afb65f26afd986200462b091d1a083
Ali Arsalan Ansari <aliarslanansari@hotmail.com>
2020-08-15 03:24:41 +0530

    added user registration validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,20 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const { check, validationResult} = require('express-validator');
 
-// @route GET api/users
+// @route POST api/users
 // @desc  Test route
 // @access Public
-router.get('/', (req, res) => { res.send('User Route'); });
+router.post("/", [
+    check('email','Please include a valid email').isEmail(),
+    check('password','Please enter a password with 6 or more characters').isLength({min:6}),
+    check('name','Name is required').not().isEmpty(),
+], (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors:errors.array()});
+    }
+    res.send("User Route");
+});
 
-module.exports = router
+module.exports = router;
--- server.js
+++ server.js
@@ -6,8 +6,12 @@ const PORT = process.env.PORT || 5000;
 //Connect Database
 connectDB();
 
+//init middlewares
+app.use(express.json({extended:false}));
+
 app.get('/', (req, res) => res.send('Api Running') );
 
+//Define Routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
