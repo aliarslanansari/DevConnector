0e1a59c7431d5c5ff4852dbb272532a7b666be60
Ali Arsalan Ansari <aliarslanansari@hotmail.com>
2020-08-15 02:48:32 +0530

    added routes

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const connectDB = require('./config/db');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-    res.send('Api Running');
-});
+//Connect Database
+connectDB();
+
+app.get('/', (req, res) => res.send('Api Running') );
+
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
