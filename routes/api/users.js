47d40e2b8163c83edcfb5dd110829edca2c60bf5
Ali Arsalan Ansari <aliarslanansari@hotmail.com>
2020-08-15 13:09:54 +0530

    added user registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,20 +1,53 @@
 const express = require("express");
 const router = express.Router();
 const { check, validationResult} = require('express-validator');
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+
+const User = require('../../models/User');
 
 // @route POST api/users
 // @desc  Test route
 // @access Public
-router.post("/", [
-    check('email','Please include a valid email').isEmail(),
-    check('password','Please enter a password with 6 or more characters').isLength({min:6}),
-    check('name','Name is required').not().isEmpty(),
-], (req, res) => {
-    const errors = validationResult(req);
-    if(!errors.isEmpty()){
-        return res.status(400).json({errors:errors.array()});
+router.post("/",[
+        check("email", "Please include a valid email").isEmail(),
+        check(
+            "password",
+            "Please enter a password with 6 or more characters"
+        ).isLength({ min: 6 }),
+        check("name", "Name is required").not().isEmpty(),
+    ], async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        const { name, email, password } = req.body;
+        try {
+            let user = await User.findOne({email});
+            if(user){
+               return res.status(400).json({ errors: [{msg:'User Already Exist'}] });
+            }
+            const avatar = gravatar.url(email,{
+                s:'200',
+                r:'pg',
+                d:'mm'
+            })
+            user = new User({
+                name,
+                email,
+                avatar,
+                password
+            });
+            const salt = await bcrypt.genSalt(10);
+            user.password = await bcrypt.hash(password,salt);
+            await user.save();
+
+            res.send("User Registered");
+        } catch (error) {
+            console.error(error.message);
+            res.status(500).send('Server error');
+        }
     }
-    res.send("User Route");
-});
+);
 
 module.exports = router;
