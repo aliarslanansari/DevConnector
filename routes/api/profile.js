f6364a20fc4e6c4faf31d00beabff85980a9ed42
Ali Zakwan <alizakwanansari@gmail.com>
2021-07-01 02:22:22 +0530

    added endpoint to delete experience from profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -180,4 +180,21 @@ router.put(
   }
 )
 
+// @route DELETE api/profile/experience/:exp_id
+// @desc  delete profile experience
+// @access Private
+router.delete('/experience/:exp_id', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id })
+    const removeIndex = profile.experience
+      .map((item) => item.id)
+      .indexOf(req.params.exp_id)
+    profile.experience.splice(removeIndex, 1)
+    await profile.save()
+    res.json(profile)
+  } catch (error) {
+    console.error(error.message)
+    res.status(500).send('Server Error')
+  }
+})
 module.exports = router
