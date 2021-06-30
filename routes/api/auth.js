b0aeba8e471d7ff318334734569c6fd66d332ab8
Ali Zakwan <alizakwanansari@gmail.com>
2021-07-01 01:21:16 +0530

    Minor typo fixed

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -25,7 +25,7 @@ router.get('/', auth, async (req, res) => {
 // @access Public
 router.post("/",[
         check("email", "Please include a valid email").isEmail(),
-        check("password", "Please is required").exists(),
+        check("password", "Password is required").exists(),
     ], async (req, res) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
