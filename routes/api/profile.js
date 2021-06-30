4befce1e1d4e06dc5dbb1310c2ae07bed916cc9d
Ali Arsalan Ansari <aliarslan1620@gmail.com>
2021-07-01 02:39:00 +0530

    create endpoint to add and delete Education from profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -197,4 +197,64 @@ router.delete('/experience/:exp_id', auth, async (req, res) => {
     res.status(500).send('Server Error')
   }
 })
+
+// @route PUT api/profile/education
+// @desc  Add profile education
+// @access Private
+router.put(
+  '/education',
+  [
+    auth,
+    [
+      check('school', 'School is required').not().isEmpty(),
+      check('degree', 'Degree is required').not().isEmpty(),
+      check('fieldOfStudy ', 'Field of Study is required').not().isEmpty(),
+      check('from', 'From date is required').not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty) {
+      return res.status(400).json({ errors: errors.array() })
+    }
+    const { school, degree, fieldofstudy, from, to, current, description } =
+      req.body
+    const newExp = {
+      school,
+      degree,
+      fieldofstudy,
+      from,
+      to,
+      current,
+      description,
+    }
+    try {
+      const profile = await Profile.findOne({ user: req.user.id })
+      profile.education.unshift(newExp)
+      await profile.save()
+      res.json(profile)
+    } catch (error) {
+      console.error(error)
+      res.status(500).send('Server Error')
+    }
+  }
+)
+
+// @route DELETE api/profile/education/:exp_id
+// @desc  delete profile education
+// @access Private
+router.delete('/education/:edu_id', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id })
+    const removeIndex = profile.education
+      .map((item) => item.id)
+      .indexOf(req.params.exp_id)
+    profile.education.splice(removeIndex, 1)
+    await profile.save()
+    res.json(profile)
+  } catch (error) {
+    console.error(error.message)
+    res.status(500).send('Server Error')
+  }
+})
 module.exports = router
