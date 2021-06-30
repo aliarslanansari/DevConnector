a1e3d871f5876735ac64abdab55e5f983000f3a1
Ali Zakwan <alizakwanansari@gmail.com>
2021-07-01 02:39:00 +0530

    create endpoint to add and delete Education from profile

--- models/Profile.js
+++ models/Profile.js
@@ -1,112 +1,112 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 const ProfileSchema = new mongoose.Schema({
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "user",
-    },
-    company: {
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+  },
+  company: {
+    type: String,
+  },
+  website: {
+    type: String,
+  },
+  location: {
+    type: String,
+  },
+  skills: {
+    type: [String],
+    required: true,
+  },
+  bio: {
+    type: String,
+  },
+  status: {
+    type: String,
+    required: true,
+  },
+  githubusername: {
+    type: String,
+  },
+  experience: [
+    {
+      title: {
         type: String,
-    },
-    website: {
-        type: String,
-    },
-    location: {
-        type: String,
-    },
-    skills: {
-        type: [String],
         required: true,
-    },
-    bio: {
+      },
+      company: {
         type: String,
-    },
-    status:{
+        required: true,
+      },
+      location: {
         type: String,
-        required:true,
-    },
-    githubusername: {
+      },
+      from: {
+        type: Date,
+        required: true,
+      },
+      to: {
         type: String,
+      },
+      current: {
+        type: Boolean,
+        default: false,
+      },
+      description: {
+        type: String,
+      },
     },
-    experience: [
-        {
-            title: {
-                type: String,
-                required: true,
-            },
-            company: {
-                type: String,
-                required: true,
-            },
-            location: {
-                type: String,
-            },
-            from: {
-                type: Date,
-                required: true,
-            },
-            to: {
-                type: String,
-            },
-            current: {
-                type: Boolean,
-                default: false,
-            },
-            description: {
-                type: String,
-            },
-        },
-    ],
-    education: [
-        {
-            school: {
-                type: String,
-                required: true,
-            },
-            degree: { 
-                type: String,
-                required:true 
-            },
-            fieldOfStudy:{
-                type: String,
-                required: true
-            },
-            from: {
-                type: Date,
-                required: true,
-            },
-            to: {
-                type: String,
-            },
-            current: {
-                type: Boolean,
-                default: false,
-            },
-            description: {
-                type: String,
-            },
-        },
-    ],
-    social:{
-        youtube:{
-            type:String
-        },
-        twitter: { 
-            type:String
-        },
-        facebook:{
-            type:String
-        },
-        linkedin:{
-            type:String
-        },
-        instagram:{
-            type:String
-        }
+  ],
+  education: [
+    {
+      school: {
+        type: String,
+        required: true,
+      },
+      degree: {
+        type: String,
+        required: true,
+      },
+      fieldofstudy: {
+        type: String,
+        required: true,
+      },
+      from: {
+        type: Date,
+        required: true,
+      },
+      to: {
+        type: String,
+      },
+      current: {
+        type: Boolean,
+        default: false,
+      },
+      description: {
+        type: String,
+      },
     },
-    date:{
-        type:Date,
-        default: Date.now
-    }
-});
+  ],
+  social: {
+    youtube: {
+      type: String,
+    },
+    twitter: {
+      type: String,
+    },
+    facebook: {
+      type: String,
+    },
+    linkedin: {
+      type: String,
+    },
+    instagram: {
+      type: String,
+    },
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+})
 
-module.exports = Profile = mongoose.model("profile", ProfileSchema);
+module.exports = Profile = mongoose.model('profile', ProfileSchema)
