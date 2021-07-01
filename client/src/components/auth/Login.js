3aa1f906f0541bd1b8a58a44dc276987b2257478
Ali Arsalan Ansari <aliarslan1620@gmail.com>
2021-07-02 01:20:43 +0530

    added login page

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,56 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Login = () => {
-  return <div>Login</div>
+  const [formData, setFormData] = React.useState({
+    email: '',
+    password: ''
+  })
+
+  const { email, password } = formData
+  const onChange = (e) =>
+    setFormData((form) => ({
+      ...form,
+      [e.target.name]: e.target.value
+    }))
+  const onSubmit = async (e) => {
+    e.preventDefault()
+    console.log('Success')
+  }
+
+  return (
+    <React.Fragment>
+      <h1 className="large text-primary">Sign In</h1>
+      <p className="lead">
+        <i className="fas fa-user"></i> Sign into your account.
+      </p>
+      <form className="form" onSubmit={onSubmit}>
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            onChange={onChange}
+            value={email}
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            minLength="6"
+            onChange={onChange}
+            value={password}
+          />
+        </div>
+        <input type="submit" className="btn btn-primary" value="Register" />
+      </form>
+      <p className="my-1">
+        Don't have an account? <Link to="/register">Sign Up</Link>
+      </p>
+    </React.Fragment>
+  )
 }
 
 export default Login
