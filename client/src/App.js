9e7d78b2489775fba94f5623e766dd8727323f10
Ali Arsalan Ansari <aliarslan1620@gmail.com>
2021-07-01 23:44:23 +0530

    added routes and code cleanup

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import React from 'react'
+import Navbar from './components/layouts/Navbar'
+import Landing from './components/layouts/Landing'
+import Register from './components/auth/Register'
+import Login from './components/auth/Login'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <React.Fragment>
+        <Navbar />
+        <Route exact path="/" component={Landing} />
+        <section className="container">
+          <Switch>
+            <Route exact path='/login' component={Login}/>
+            <Route exact path='/register' component={Register}/>
+          </Switch>
+        </section>
+      </React.Fragment>
+    </Router>
+  )
 }
 
-export default App;
+export default App
