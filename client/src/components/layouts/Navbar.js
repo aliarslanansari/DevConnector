056955d9991992cd5701471940ea2d1616daa099
Ali Arsalan Ansari <aliarslan1620@gmail.com>
2021-07-03 23:56:16 +0530

    added profiles list

--- client/src/components/layouts/Navbar.js
+++ client/src/components/layouts/Navbar.js
@@ -10,6 +10,9 @@ const Navbar = ({ auth, logout }) => {
   const authLinks = (
     <ul>
       <li>
+        <Link to="/profiles">Developers </Link>
+      </li>
+      <li>
         <Link to="/dashboard">
           <i className="fas fa-user"></i>
           <span className="hide-sm"> Dashboard</span>
@@ -25,6 +28,9 @@ const Navbar = ({ auth, logout }) => {
   )
   const guestLinks = (
     <ul>
+      <li>
+        <Link to="/profiles">Developers </Link>
+      </li>
       <li>
         <Link to="#!">Developers</Link>
       </li>
