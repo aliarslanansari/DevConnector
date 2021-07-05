18f0076e8c36740838552ab4eb7b2e0d27621628
Ali Arsalan Ansari <aliarslan1620@gmail.com>
2021-07-05 21:43:50 +0530

    minor type fix in login component

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -50,7 +50,7 @@ const Login = ({ login, isAuthenticated }) => {
             value={password}
           />
         </div>
-        <input type="submit" className="btn btn-primary" value="Register" />
+        <input type="submit" className="btn btn-primary" value="Login" />
       </form>
       <p className="my-1">
         Don't have an account? <Link to="/register">Sign Up</Link>
